ac9522d6a6e28de5eb15ade6118933f41f6aa500
lihuang-zheng <jerryzheng0131@gmail.com>
2020-03-27 10:26:36 -0500

    added api base url, and server config for credentials.

--- api/server.js
+++ api/server.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const jokesRouter = require('../jokes/jokes-router.js');
+const authenticate = require("../auth/authenticate-middleware.js");
+const authRouter = require("../auth/auth-router.js");
+const jokesRouter = require("../jokes/jokes-router.js");
 
 const server = express();
 
+const serverConfig = {
+  origin: "http://localhost:1234",
+  credentials: true
+};
+
 server.use(helmet());
-server.use(cors());
+server.use(cors(serverConfig));
 server.use(express.json());
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/jokes", authenticate, jokesRouter);
+
+server.use("/api", (req, res) =>
+  res.status(200).json({ message: "Welcome, this is the Dad Jokes API." })
+);
 
 module.exports = server;
